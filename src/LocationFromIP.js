2a9960ce349ceb8e1a46698baf64cb15dff48bde
smm7530 <97692542+smm7530@users.noreply.github.com>
2022-01-30 17:04:04 -0500

    Add files via upload

--- src/LocationFromIP.js
+++ src/LocationFromIP.js
@@ -1,6 +1,7 @@
 // dependencies / things imported
 import { LitElement, html, css } from 'lit';
-import { UserIP } from './UserIP.js';
+import { UserIP } from './UserIP.js'
+import  '@lrnwebcomponents/wikipedia-query/wikipedia-query.js';
 
 export class LocationFromIP extends LitElement {
   static get tag() {
@@ -12,12 +13,16 @@ export class LocationFromIP extends LitElement {
     this.locationEndpoint = 'https://freegeoip.app/json/';
     this.long = null;
     this.lat = null;
+    this.city = null;
+    this.state = null;
   }
 
   static get properties() {
     return {
       long: { type: Number },
       lat: { type: Number },
+      city: { type: String } ,
+      state: { type: String }
     };
   }
 
@@ -42,6 +47,8 @@ export class LocationFromIP extends LitElement {
         console.log(data);
         this.long = data.longitude;
         this.lat = data.latitude;
+        this.city = data.city;
+        this.state = data.region_name;
         return data;
       });
   }
@@ -56,15 +63,23 @@ export class LocationFromIP extends LitElement {
           height: 500px;
           width: 500px;
         }
-      `,
+         
+        `,
     ];
   }
+
 
   render() {
     // this function runs every time a properties() declared variable changes
     // this means you can make new variables and then bind them this way if you like
     const url = `https://maps.google.com/maps?q=${this.lat},${this.long}&t=&z=15&ie=UTF8&iwloc=&output=embed`;
-    return html`<iframe title="Where you are" src="${url}"></iframe> `;
+    return html`<iframe title="Where you are" src="${url}"></iframe>
+                <a href = "https://www.google.com/maps/@40.804,77.910,14z">a map link</a> 
+                <wikipedia-query search = ${this.city}></wikipedia-query>
+                <wikipedia-query search = "${this.city}, ${this.state}"></wikipedia-query>
+                <wikipedia-query search = ${this.state}></wikipedia-query>`;
+
+
   }
 }
 
